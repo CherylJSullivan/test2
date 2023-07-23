2969ca4a2c6d44768a33983a75c79975d31a455b
CherylJSullivan <140143510+CherylJSullivan@users.noreply.github.com>
2023-07-23 08:00:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,11 +11,22 @@ actions = [
     ((681, 530), None),  # install
     ((506, 499), None),
     ((154, 379), None),  # device
+    ((154, 379), None),  # device
     ((331, 282), None),  # Log in
     ((458, 333), os.getenv("EMAIL_SECRET")), # email (type email secret from env after clicking)
     ((449, 375), os.getenv("PASSWORD_SECRET")), # pass (type password secret from env after clicking)
     ((518, 435), None)  # log in
 ]
+
+# Function to perform the taskkill operation with retries
+def taskkill_process(process_name, max_retries=5, retry_delay=5):
+    for _ in range(max_retries):
+        try:
+            subprocess.run(["taskkill", "/f", "/im", process_name])
+            return  # Exit the function if the taskkill is successful
+        except subprocess.CalledProcessError:
+            print(f"Failed to kill {process_name}. Retrying in {retry_delay} seconds.")
+            time.sleep(retry_delay)
 
 # Wait for a few seconds to give time to focus on the target application
 time.sleep(5)
@@ -29,5 +40,7 @@ for pos, text in actions:
 
     # Check if the current action is to close Microsoft Edge
     if pos == (506, 499):
-        # Execute the "taskkill" command to close Microsoft Edge
-        subprocess.run(["taskkill", "/f", "/im", "MicrosoftEdge.exe"])
+        # Execute the "taskkill" command to close Microsoft Edge with retries
+        taskkill_process("msedge.exe")
+
+# Additional actions here if needed

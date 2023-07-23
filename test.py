ed31e86633cadef6ef8cc4b5fcd586c51362c7c6
CherylJSullivan <140143510+CherylJSullivan@users.noreply.github.com>
2023-07-23 10:27:40 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Set your environment variables
-os.environ["EMAIL_SECRET"] = "secret_email_value"
-os.environ["PASSWORD_SECRET"] = "secret_password_value"
-
-# Optional: Print the environment variables to confirm their values
-print(os.environ["EMAIL_SECRET"])
-print(os.environ["PASSWORD_SECRET"])
